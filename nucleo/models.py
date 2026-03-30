94b5e2aeb3a913668dcb1364c054683fe8b85ea3
Miguel Ramos <return.infox@gmail.com>
2026-03-30 20:08:12 -0300

    modelos creados para ver el content type

--- nucleo/models.py
+++ nucleo/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
-# Create your models here.
+
+
 class Comentario(models.Model):
     texto = models.TextField(max_length=500) # sql varchar(500)
     autor = models.CharField(max_length=100) # sql varchar(100)
@@ -20,3 +21,36 @@ class Comentario(models.Model):
 
     def __str__(self):
         return f"Comentario ID: {self.id} -> {self.content_type} ({self.object_id})"
+    
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100) # sql varchar(100)
+    precio = models.DecimalField(max_digits=10, decimal_places=2) # sql decimal(10,2)
+    descripcion = models.TextField(max_length=1000) # sql varchar(1000)
+
+    #* a continuar lo relacionado con content type
+    # consulta inversa o (reverse lookup)
+    comentarios = GenericRelation(Comentario)
+    # Select * from comentarios where content_type_id = <id de producto> and object_id = <id del producto especifico>
+    #* Fin de lo relacionado con content type
+    class Meta:
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
+    
+    def __str__(self):
+        return self.nombre
+    
+class ArticuloBlog(models.Model):
+    titulo = models.CharField(max_length=200) # sql varchar(200)
+    contenido = models.TextField() # sql text
+
+    #* a continuar lo relacionado con content type
+    # consulta inversa o (reverse lookup)
+    comentarios = GenericRelation(Comentario)
+    #* Fin de lo relacionado con content type
+
+    class Meta:
+        verbose_name = 'Artículo de Blog'
+        verbose_name_plural = 'Artículos de Blog'
+    
+    def __str__(self):
+        return self.titulo
